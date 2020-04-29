1c5def204379566ac0197ac5de737e86a739e001
Aaron Greenspan <afg419@gmail.com>
2020-04-29 11:58:04 -0600

    adds subject interface

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,27 @@
 import { Plugins } from '@capacitor/core'
 import { TorPlugin } from './definitions'
+import { Subject } from 'rxjs'
 const { TorPlugin : TorNative } = Plugins;
 
 // Provides TS type safety for calling code.
 export class Tor implements TorPlugin {
   constructor() {}
 
+  // Plugins.TorPlugin.addListener("torInitProgress", info => {
+  //   console.log(`Initted Tor logs: ${JSON.stringify(info)}`)
+  // })
+  // await this.tor.initProgress.subscribe((info) => {
+
+  // })
+
+  initProgress: Subject<number> = new Subject()
+  eventListener: any
+
   initTor(opt?: { socksPort: number }): Promise<void> {
+    const eventListener = TorNative.addListener("torInitProgress", info => {
+      this.initProgress.next(info.progress)
+      if(Number(info.progress) >= 100) { eventListener.remove() }
+    })
     return TorNative.initTor(opt)
   }
 }
