a9a82af50c633fbbe1de40b1bae037ab0e8e58c0
Keagan McClelland <keagan.mcclelland@gmail.com>
2020-05-30 14:44:24 -0600

    fragile type hack that compensates for interfaces not exported from capacitor

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,15 +1,28 @@
 import { Observable } from "rxjs";
+import { PluginListenerHandle } from '@capacitor/core'
 
 // @ts-ignore
 declare module "@capacitor/core" {
   interface PluginRegistry {
-    TorPlugin: TorPlugin;
+    TorPlugin: TorNative;
   }
 }
 
 export interface TorPlugin {
   start(opt?: { socksPort: number, initTimeout?: number }): Observable<number>
   stop()   : Promise<void>
+  reconnect(): Promise<void>
   newnym() : Promise<void>
   isRunning(): Promise<boolean>
+}
+
+export interface TorNative extends Plugin {
+  start(opt?: { socksPort: number, initTimeout?: number }): Observable<number>
+  stop()   : Promise<void>
+  reconnect(): Promise<void>
+  newnym() : Promise<void>
+  isRunning(): Promise<{ running: boolean }>
+}
+export interface Plugin {
+  addListener(eventName: string, listenerFunc: Function): PluginListenerHandle;
 }
